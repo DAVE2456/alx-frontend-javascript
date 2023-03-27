9644009aec10aa3c5502279bf806a7b0d466367e
DAVE2456 <davehunegnaw@gmail.com>
2023-03-27 16:41:30 +0300

    initial commit

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,7 @@ export default function updateStudentGradeByCity(listStudents, city, newGrades) 
   for (const student of students) {
     student.grade = 'N/A';
   }
-  const newstudents = student.map ((student) => {
+  const newSXStudents = student.map ((student) => {
     const single = student;
     for (const grade of newGrades) {
       if (single.id === grade.studentId) {
