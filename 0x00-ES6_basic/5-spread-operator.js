45441243536fe8801df5883be10235843c0c0601
DAVE2456 <davehunegnaw@gmail.com>
2023-04-05 17:31:52 +0300

    correction code

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
-export default function concatArrays(array2, array3, string) {                
+export default function concatArrays(array1, array2, string) {                
   return [...array2, ...array3, ...string];                                           
 }
    
