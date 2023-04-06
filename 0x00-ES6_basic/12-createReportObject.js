d714af389c6270f81ebeb1296c35d1f66ff097d8
DAVE2456 <davehunegnaw@gmail.com>
2023-04-06 16:06:36 +0300

    correction code

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments: (output) => object.keys(output) length,
+    getNumberOfDepartments: (output) => object.keys(output)length,
   };
 }
