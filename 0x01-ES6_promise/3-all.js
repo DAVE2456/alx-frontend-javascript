84cd99672644629467a07a13e5eb58e158c8467c
DAVE2456 <davehunegnaw@gmail.com>
2023-04-07 11:13:35 +0300

    correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { createUser, uploadphoto } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-  return promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       const { body } = values[0];
       const { firstName, lastName } = values[1];
-      
+
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
