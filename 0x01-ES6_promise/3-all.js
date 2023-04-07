7ac062f3a912d8d57ac9ab8c923e5fe5a51b8e3c
DAVE2456 <davehunegnaw@gmail.com>
2023-04-07 10:55:57 +0300

    initial commit

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { createUser, uploadphoto } from './utils';
+finction handleProfileSignup() {
+  return promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName, lastName } = values[1];
+      
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handleProfileSignup;
